2667441374cfdf8b98dfcbbe54bc2f1ba339a5d1
Emile Durkheim <email@email.com>
2023-04-09 19:47:06 +0200

    added username field to register form, html

--- core/forms.py
+++ core/forms.py
@@ -4,10 +4,17 @@ class LoginForm(forms.Form):
     email = forms.EmailField(max_length=255,
                              widget=forms.EmailInput(attrs={'class': 'form-text', 
                                                             'placeholder': 'E-Mail'}))
+
     password = forms.CharField(max_length=255,
                                widget=forms.PasswordInput(attrs={'class': 'form-text', 
                                                                  'placeholder': 'Passwort'}))
 
 
 class RegisterForm(LoginForm):
-    password2 = password
+    username = forms.CharField(max_length=20,
+                               widget=forms.TextInput(attrs={'class': 'form-text', 
+                                                             'placeholder': 'Nutzername'}))
+
+    password2 = forms.CharField(max_length=255,
+                               widget=forms.PasswordInput(attrs={'class': 'form-text', 
+                                                                 'placeholder': 'Passwort wiederholen'}))
